092add8a3fd70af900f613a077d2700f19178c48
Quas <aaron.hickman201@gmail.com>
2023-08-03 16:09:37 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+let hangWord = '';
 
 let renderWord = () => {
-    for(let i = 0; i < workerData.length; i++) {
-        document.createElement('div')
+    let html = '';
+    for (let i = 0; i < hangWord.length; i++) {
+        html += `
+        <div id="word-div">${hangWord[i]}</div>
+        `;
     }
-}
+    document.getElementById('box-div').innerHTML = html;
+};
 
 let generateWord = () => {
     fetch('https://random-word-api.vercel.app/api?words=1')
         .then(res => res.json())
-        .then(data => console.log(data))
-}
-
+        .then(data => {
+            hangWord = data[0];
+            renderWord();
+        });
+};
 
 generateWord();
